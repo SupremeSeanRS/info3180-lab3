4713a599ffe34f551678a8c2e36b0cc486a7945b
SupremeSeanRS <seanaung.21@gmail.com>
2020-02-15 23:32:42 -0500

    completed lab3

--- app/views.py
+++ app/views.py
@@ -7,6 +7,9 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+from app.forms import ContactForm
+from app import mail
+from flask_mail import Message
 
 
 ###
@@ -23,6 +26,20 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
+
+@app.route('/contact/', methods=['GET', 'POST'])
+def contact():
+    """Render the website's contact page"""
+    form = ContactForm()
+    if form.validate_on_submit():
+        msg = Message(form.subject.data,
+                      sender=(form.name.data , form.email.data),
+                      recipients=["john@uwimail.edu"])
+        msg.body = form.message.data
+        mail.send(msg)
+        flash(u'Message was successfully sent!')
+        return redirect('/')
+    return render_template('contact.html', form=form)
 
 
 ###
